3e50b5dfcf114fae34effaff79d5c078a8fe29aa
programmermosharof <mosharof.dev@gmail.com>
2026-02-04 15:38:51 +0600

    JavaScript Syntax

---
+++ Syntax.js
@@ -0,0 +1,15 @@
+let name  = " My Name is Mosharof";
+
+console.log(name);
+
+
+let age = 20;
+age = 23;
+
+console.log(age);
+
+
+if(true){
+    console.log("Hello");
+    console.log("Hi I am Mosharof");
+}
---
+++ variable.js
@@ -0,0 +1,10 @@
+let city = "Dhaka";
+
+console.log(city);
+
+
+let number = 50 ;
+
+number = 80;
+
+console.log(number);
