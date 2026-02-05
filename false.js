69160fe4f843cad30d31defef1f88ad6f034b8ed
programmermosharof <mosharof.dev@gmail.com>
2026-02-05 23:51:24 +0600

    Error fixed

---
+++ false.js
@@ -0,0 +1,19 @@
+let name = "";
+if(name){
+    console.log("Name ache");
+}else{
+    console.log("Name nai");
+}
+
+let age = 0 ;
+if(age){
+    console.log("Age ache");
+}else{
+    console.log("age nai");
+}
+
+let ag = null;
+console.log( ag);
+
+let z = undefined;
+console.log(z);
