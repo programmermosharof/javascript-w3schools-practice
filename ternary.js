363b135871cb7a350a5546a83bf46824ef48582f
programmermosharof <mosharof.dev@gmail.com>
2026-02-06 00:05:48 +0600

    ternary complete

---
+++ ternary.js
@@ -0,0 +1,16 @@
+let age = 10;
+
+
+age >= 18 ? console.log("Adult"): console.log("child");
+
+let isLoggedIn = true ;
+
+isLoggedIn ? console.log("welcome") : console.log("Please login");
+
+let num =8 ;
+
+num % 2 === 0  ? console.log("Even") : console.log("odd");
+
+
+let ages = 20;
+ages >= 20 ? console.log("Allowed") : console.log("Not Allowed");
