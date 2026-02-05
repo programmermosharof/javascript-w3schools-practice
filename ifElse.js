c74c7de29b5b2945ba880e3849ea039a0d45fb99
programmermosharof <mosharof.dev@gmail.com>
2026-02-05 19:22:46 +0600

    if else my learning complete

---
+++ ifElse.js
@@ -0,0 +1,49 @@
+
+// task 1
+
+let number = 70;
+
+if(number >= 50){
+    console.log("Congreats tumi 50% discount Peyecho");
+}
+// task2
+
+let isLoggedIn = true;
+if(isLoggedIn === true){
+    console.log("Welcome");
+}
+// task1
+let even = 29;
+if(even  % 2 === 0 ){
+console.log("Even");
+}else{
+    console.log("Odd");
+}
+
+// task2
+let atmBoth = 500;
+if(atmBoth >= 1000){
+    console.log("Withdraw allowed");
+}else{
+    console.log("Low balance");
+}
+
+
+let num = 30;
+if(num > 0){
+    console.log("Positive");
+}else if (num < 0){
+    console.log("Negative");
+}else {
+    console.log("Zero");
+}
+// Task 2
+
+let age = 10;
+if(age >= 60){
+    console.log("Senior");
+}else if (age >= 18){
+    console.log("Adult");
+}else {
+    console.log("Child");
+}
