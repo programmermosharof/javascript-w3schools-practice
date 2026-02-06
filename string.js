d15473dd9f3549a3adbe4bb3424cd2fe19bdd2ac
programmermosharof <mosharof.dev@gmail.com>
2026-02-06 15:18:12 +0600

    string basic complete

---
+++ string.js
@@ -0,0 +1,55 @@
+let text = "Hello My Name is Mosharof"
+
+console.log(text.length);
+
+console.log(text[6]);
+
+let js = "JavaScript";
+
+console.log(js.length);console.log(js[0]);
+
+// String Templates
+
+let name = "Mosharof";
+
+let msg = "Hello " + name + ", Welcome";
+
+console.log(msg);
+
+let names = "Fahim";
+
+let sms = `Hello ${names}, Welcome!`;
+console.log(sms);
+
+let num = "Laddo";
+
+let  result = `Hi Fahim tumi exam e ${num} Paso `;
+
+console.log(result);
+
+
+let user = "Rahim";
+
+let blance = 500;
+
+let texts = `Hello ${user}, your Blance is ${blance}, taka`;
+
+console.log(texts);
+
+
+// Task Te............'
+
+
+let use = "Mosharof";
+
+let unread = 3;
+
+let allSms = `Hello ${use}, you have ${unread}, unread messages.`;
+
+console.log(allSms);
+
+
+let mul = `Welcome Mosharof
+You have ${unread} new notifications`;
+
+console.log(mul);
