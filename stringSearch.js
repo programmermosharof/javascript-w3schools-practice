5e31c20be597d691b775c151d95913fc881aed8b
programmermosharof <mosharof.dev@gmail.com>
2026-02-06 16:48:29 +0600

    string all topic complete

---
+++ stringSearch.js
@@ -0,0 +1,35 @@
+let text = "I love JavaScript";
+
+console.log(text.includes("vaS"));
+
+console.log(text.includes("Python"));
+
+let texts = "Hello World";
+
+console.log(texts.indexOf("World")); 
+
+let username = "admin123";
+console.log(username.startsWith("admin"));
+console.log(username.startsWith("user"));
+
+
+let file = "photo.png";
+
+console.log(file.endsWith(".png"));
+
+console.log(file.endsWith(".svg"));
+
+let email = "mosharof.dev@gmail.com";
+
+console.log(email.length[-1]);
+
+console.log(email.includes("@"));
+console.log(email.startsWith("mosharof"));
+
+console.log(email.endsWith(".com"));
+
+let word = "Hello";
+
+console.log(word.length);          // 5
+console.log(word.length - 1);      // 4
+
