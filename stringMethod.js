7852641258c08903b0c8c037c15aa552139c072f
programmermosharof <mosharof.dev@gmail.com>
2026-02-06 15:56:51 +0600

    stringMethod

---
+++ stringMethod.js
@@ -0,0 +1,41 @@
+let text = "hello mosharof";
+
+let result = text.toUpperCase();
+
+console.log(result);
+
+
+let email = "MOSHAROF.DEV@GMAIL.COM";
+
+console.log(email.toLowerCase());
+
+
+let input = "  Hello  ";
+console.log(input.trim());
+
+let word = "JavaScripts";
+
+console.log(word.slice(4, 10));
+
+
+let msg = "I love JS";
+
+console.log(msg.replace("JS", "JavaScript"));
+
+
+// Ja string Method
+
+let username = " Mosharof ";
+
+console.log(username.trim());
+
+
+let convert = "hi my name is mosharof";
+
+console.log(convert.toUpperCase());
+
+let character = "JavaScript";
+
+console.log(character.slice(0, 4));
+
+
