8520f699c493195a41c41d938a44899be520809b
programmermosharof <mosharof.dev@gmail.com>
2026-02-06 07:45:17 +0600

    basic loop complete

---
+++ loop.js
@@ -0,0 +1,18 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+for(let justEven = 1; justEven < 10 ; justEven ++ ){
+    if(justEven % 2 === 0){
+     console.log(justEven);
+    }
+   
+}
+
+
+let sum = 0;
+
+for (let m = 1; m < 10 ; m++){
+    sum = sum + m;
+}
+console.log(sum);
