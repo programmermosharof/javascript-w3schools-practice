95d431a12940bea2966401d4b7cfe96959232e43
programmermosharof <mosharof.dev@gmail.com>
2026-02-07 16:19:47 +0600

    Number Method is complete

---
+++ numMethod.js
@@ -0,0 +1,36 @@
+// let num = 5000;
+// let str = num.toString();
+
+// console.log(typeof str);
+
+// let names = "Mosharof";
+
+// console.log(Number.isInteger(names));
+
+// let checkNumber = 70;
+
+// console.log(Number.isNaN(70));
+// Task 1 
+let number = 12.3456 ;
+
+console.log(number.toFixed(2));
+
+
+// Task 2
+let string = Number("40")
+
+
+
+// tsk 3 
+
+let max = Number.MAX_VALUE;
+console.log(max.toFixed(2));
+
+let dot = Number.MIN_VALUE;
+
+console.log(dot );
+
+
+let result = "a, bd " * 4;
+
+console.log(result);
---
+++ number.js
@@ -0,0 +1,20 @@
+// let number  = 40.99;
+// console.log(parseInt(number));
+
+// let numbers = 60.700000;
+
+// console.log(parseFloat(numbers));
+// console.log(numbers.toFixed(2));
+
+// task 1 
+
+let num = "50";
+let number = 10;
+
+let total = Number(num) + number;
+
+console.log(total);
+
+let numbers = "20.5";
+
+console.log(parseInt(numbers));
