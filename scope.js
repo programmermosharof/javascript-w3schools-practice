6da1060ecf15076ba9593a0e05f22498cb476ff3
programmermosharof <mosharof.dev@gmail.com>
2026-02-10 00:19:44 +0600

    all task complete today

---
+++ scope.js
@@ -0,0 +1,28 @@
+let name = "Global";
+
+function test(){
+    let age = 22;
+    console.log(name);
+    console.log(age);
+}
+test();
+
+let x = 5;
+function demo(){
+    console.log(x);
+}
+demo();
+
+if(true){
+    let a = 5;
+    console.log(a);
+}
+
+for(let i = 0; i< 2; i++){
+    console.log(i);
+}
+
+sayHi();
+function sayHi(){
+    console.log("Hi");
+}
