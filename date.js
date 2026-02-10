c001d9d589b950e58609db835d7b6530bffdb366
programmermosharof <mosharof.dev@gmail.com>
2026-02-10 11:40:46 +0600

    date topic is complete

---
+++ date.js
@@ -0,0 +1,21 @@
+// const nows = new Date();
+
+// const dat =
+//   now.getDate() + "/" +
+//   (now.getMonth() + 1) + "/" +
+//   now.getFullYear();
+
+// console.log(dat);
+
+
+const now = new Date();
+const date = 
+now.getDate() + "/" +  " Month" +
+(now.getMonth() + 1)  + " Year" +
+now.getFullYear();
+
+console.log(date);
+
+
+const birthday = new Date("2006-01-14");
+console.log(birthday);
